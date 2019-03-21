b7f6d40551d3e45111a412130dc6c6baa7b5ccd0
Saritha <Saritha@10.0.0.234>
2019-03-21 13:43:13 -0400

    added some functions to it

--- TestProject/src/com/newproject/FirstProject.java
+++ TestProject/src/com/newproject/FirstProject.java
@@ -6,9 +6,13 @@ public class FirstProject {
 		
 		
 		String s="hello world";
+		String p="hell world";
+		
 		int i;
 		System.out.println(s);
 		System.out.println(s.substring(5,11));
+		System.out.println(s.compareTo(p));
+		System.out.println(s.concat(p));
 		for(i=s.length()-1;i>=0;i--)
 		{
 			System.out.print(s.charAt(i));
