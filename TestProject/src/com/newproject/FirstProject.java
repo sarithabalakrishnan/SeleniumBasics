45557061ae167f1300257489c9ab8f15d0a44d5c
Saritha <Saritha@10.0.0.234>
2019-03-21 13:19:03 -0400

    Created a package and added class

---
+++ TestProject/src/com/newproject/FirstProject.java
@@ -0,0 +1,19 @@
+package com.newproject;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		
+		
+		String s="hello world";
+		int i;
+		System.out.println(s);
+		System.out.println(s.substring(5,11));
+		for(i=s.length()-1;i>=0;i--)
+		{
+			System.out.print(s.charAt(i));
+		}
+		
+	}
+
+}
