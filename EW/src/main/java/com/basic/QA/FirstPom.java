2ed8d1b809286d6241de5e70c1fc0c7a4ffcf85d
Saritha <Saritha@10.0.0.234>
2019-03-24 14:45:50 -0400

    New pom project

---
+++ EW/src/main/java/com/basic/QA/FirstPom.java
@@ -0,0 +1,11 @@
+package com.basic.QA;
+
+public class FirstPom {
+	
+	public static String Firstprgm(String Name)
+	{
+		System.out.println("This is my first program in "+Name);
+		return Name;
+	}
+
+}
---
+++ EW/src/test/java/com/basic/test/FirstMavenTest.java
@@ -0,0 +1,13 @@
+package com.basic.test;
+
+import com.basic.QA.FirstPom;
+
+public class FirstMavenTest extends FirstPom {
+
+	public static void main(String[] args) {
+		FirstMavenTest test = new FirstMavenTest();
+		test.Firstprgm("IN Page Object Model");
+
+	}
+
+}
